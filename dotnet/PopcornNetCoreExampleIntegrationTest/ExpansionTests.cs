6a3b1b8059573f8a07a5e8576f20d2e71401cc93
SKYWARDAPPS\abarbato <abarbato@skywardapps.com>
2017-11-16 15:37:49 -0800

    #17920 Finishing up the first go of adding integration tests

--- dotnet/PopcornNetCoreExampleIntegrationTest/ExpansionTests.cs
+++ dotnet/PopcornNetCoreExampleIntegrationTest/ExpansionTests.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using PopcornNetCoreExample;
 using PopcornNetCoreExample.Models;
 using PopcornNetCoreExample.Projections;
 using PopcornNetCoreExample.Wire;
 using PopcornNetCoreExampleIntegrationTest._Utilities;
 using Shouldly;
 using Skyward.Popcorn;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace PopcornNetCoreExampleIntegrationTest
 {
@@ -54,6 +57,59 @@ namespace PopcornNetCoreExampleIntegrationTest
             result.Any(c => c.StreetAddress != null).ShouldBeTrue();
             result.Any(c => c.ZipCode != 0).ShouldBeTrue();
             result.Any(c => c.Employees != new List<Employee>()).ShouldBeTrue();
+        }
+
+        // A factory generated property comes through despite not being included
+        [TestMethod]
+        public async Task FactoryGeneratedPropertyNotRequested()
+        {
+            var response = await TestSetup.Client.GetAsync(Utilities.employeesRelUrl + $"?include=[{nameof(EmployeeProjection.FullName)}, {nameof(EmployeeProjection.InsuredVehicles)}]");
+
+            // convert the response
+            string responseBody = await response.Content.ReadAsStringAsync();
+            var json = JsonConvert.DeserializeObject<Response>(responseBody);
+            var result = JsonConvert.DeserializeObject<List<EmployeeProjection>>(json.Data.ToString());
+
+            // Assert that the mapping applied accurately and the desired results came through
+            result.Any(c => c.FirstName != null).ShouldBeFalse();
+            result.Any(c => c.FullName == null).ShouldBeFalse();
+
+            result.Any(c => c.Employment != EmploymentType.Employed).ShouldBeFalse();
+        }
+
+        // A factory generated property does not overwrite the actual database value
+        [TestMethod]
+        public async Task FactoryGeneratedPropertyRequested()
+        {
+            var response = await TestSetup.Client.GetAsync(Utilities.employeesRelUrl + $"?include=[{nameof(EmployeeProjection.FullName)}, {nameof(EmployeeProjection.FirstName)}, {nameof(EmployeeProjection.Employment)}]");
+
+            // convert the response
+            string responseBody = await response.Content.ReadAsStringAsync();
+            var json = JsonConvert.DeserializeObject<Response>(responseBody);
+            var result = JsonConvert.DeserializeObject<List<EmployeeProjection>>(json.Data.ToString());
+
+            // Assert that the mapping applied accurately and the desired results came through
+            result.Any(c => c.LastName != null).ShouldBeFalse();
+            result.Any(c => c.FullName == null).ShouldBeFalse();
+
+            result.FirstOrDefault(n => n.FirstName == "Liz").Employment.ShouldBe(EmploymentType.FullTime);
+            result.FirstOrDefault(n => n.FirstName == "Jack").Employment.ShouldBe(EmploymentType.PartTime);
+        }
+
+        // A factory generated property comes through despite not being included
+        [TestMethod]
+        public async Task TranslatedProperty()
+        {
+            var response = await TestSetup.Client.GetAsync(Utilities.employeesRelUrl + $"?include=[{nameof(EmployeeProjection.Birthday)}]");
+
+            // convert the response
+            string responseBody = await response.Content.ReadAsStringAsync();
+            var json = JsonConvert.DeserializeObject<Response>(responseBody);
+            var result = JsonConvert.DeserializeObject<List<EmployeeProjection>>(json.Data.ToString());
+
+            // Assert that the mapping applied accurately and the desired results came through
+            result.Any(c => c.FirstName != null).ShouldBeFalse();
+            result.Any(c => DateTimeOffset.Parse(c.Birthday).ToString("MM/dd/yyyy") != c.Birthday).ShouldBeFalse();
         }
 
         // An include on simple properties returns properly
@@ -128,6 +184,54 @@ namespace PopcornNetCoreExampleIntegrationTest
             // Assert failure and appropriate error
             json.Success.ShouldBeFalse();
             json.ErrorCode.ShouldBe(typeof(SelfReferencingLoopException).FullName);
+        }
+
+        // An include including an invalid property returns a proper error
+        [TestMethod]
+        public async Task IncludeNonExistentProperty()
+        {
+            var response = await TestSetup.Client.GetAsync(Utilities.carsRelUrl + $"?include=[{nameof(CarProjection.Insured)}, {nameof(CarProjection.Year)}, {nameof(CarProjection.Make)}, Fishy]");
+
+            // convert the response
+            string responseBody = await response.Content.ReadAsStringAsync();
+            var json = JsonConvert.DeserializeObject<ErrorResponse>(responseBody);
+
+            json.Success.ShouldBeFalse();
+            json.ErrorCode.ShouldBe(typeof(ArgumentOutOfRangeException).FullName);
+            json.ErrorMessage.ShouldContain("Specified argument was out of the range of valid values.\r\nParameter name: Fishy");
+        }
+
+        // A complete null response object returned
+        [TestMethod]
+        public async Task ResponseObjectNull()
+        {
+            var response = await TestSetup.Client.GetAsync(Utilities.nullRelUrl);
+
+            // convert the response
+            string responseBody = await response.Content.ReadAsStringAsync();
+            var json = JsonConvert.DeserializeObject<Response>(responseBody);
+
+            // Assert failure and appropriate error
+            json.Success.ShouldBeTrue();
+            json.Data.ShouldBeNull();
+        }
+
+        // A complete error response object returned from a server side error (i.e. not Popcorn itself)
+        [TestMethod]
+        public async Task ResponseObjectServerError()
+        {
+            var response = await TestSetup.Client.GetAsync(Utilities.errorRelUrl);
+
+            // convert the response
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Assert.IsTrue(response.StatusCode == System.Net.HttpStatusCode.Moved);
+
+            var json = JsonConvert.DeserializeObject<ErrorResponse>(responseBody);
+
+            // Assert failure and appropriate error
+            json.Success.ShouldBeFalse();
+            json.ErrorCode.ShouldBe(typeof(ArgumentException).FullName);
+            json.ErrorMessage.ShouldBe("This is a test exception");
         }
     }
 }
